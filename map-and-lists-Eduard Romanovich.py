f6202680ec89a3b3fb2e3b665464f2729218ad80
EduardRomanovich <35576317+EduardRomanovich@users.noreply.github.com>
2018-02-01 01:47:53 -0600

    Add files via upload

---
+++ map-and-lists-Eduard Romanovich.py
@@ -0,0 +1,16 @@
+map-and-lists-eduardromanovich
+"""
+Created on Thu Jan 31 23:21:20 2018
+
+@author: Eduard
+"""
+
+a=[]
+a.append([0,1,3,5,7,9,11,13])
+print(a[0])
+b=a[0]
+print(b)
+print(b[0:4])
+for loopElem in b:
+	print(loopElem)
+    
